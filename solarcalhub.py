c644b4b38ab6b6aff9586869186ca31a72325731
jayson <miles@gzsolartech.com>
2018-12-02 17:58:01 +0800

    平均值，变化率标准差

--- solarcalhub.py
+++ solarcalhub.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python3
 
 import connexion
+from connexion import NoContent
 import numpy as np
 
 
-def post_greeting(name: str) -> str:
-    inputstr = str.split(",")
+def post_mean(array: str) -> str:
+    inputstr = array.split(",")
     arr = []
     for numi in inputstr:
         try:
             arr.append(float(numi))
         except ValueError:
-            continue
+            return NoContent, 404
     # 求均值
-    name = np.mean(arr)
-    return '{name}'.format(name=name)
+    calresult = np.mean(arr)
+    return '{result}'.format(result=calresult)
+
+def post_rocsd(array: str) -> str:
+    inputstr = array.split(",")
+    arr = []
+    for numi in inputstr:
+        try:
+            arr.append(float(numi))
+        except ValueError:
+            return NoContent, 404
+    # 计算变化率
+    result = []
+    it = iter(arr)
+    n1 = float(it.__next__())
+    for n2 in it:
+        result.append((n2 - n1) / n1)
+        n1 = float(n2)
+    # 求变化率标准差
+    calresult = np.std(result, ddof=1)
+    return '{result}'.format(result=calresult)
 
 if __name__ == '__main__':
     app = connexion.FlaskApp(__name__, port=9090, specification_dir='swagger/')
-    app.add_api('helloworld-api.yaml', arguments={'title': 'Hello World Example'})
+    app.add_api('solarcalhub-api.yaml', arguments={'title': 'Solar Calculation Hub'})
     app.run()
