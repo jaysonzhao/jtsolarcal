78da4be01da12f64c1c82dc7f889e222ec6cdb0d
Jaysonzhao <jaysonzhao@vip.qq.com>
2019-10-10 21:47:29 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -156,5 +156,5 @@ def post_kurtosis(array: list):
 if __name__ == '__main__':
     app = connexion.FlaskApp(__name__, port=8080, specification_dir='swagger/')
     hostname = socket.gethostname()
-    app.add_api('solarcalhub-api.yaml', arguments={'title': 'Solar Calculation Hub', 'host': hostname+':8080'})
+    app.add_api('solarcalhub-api.yaml', arguments={'title': 'Demo Calculation Hub', 'host': hostname+':8080'})
     app.run()
